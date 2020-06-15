94e8c360bebe15879ccb6ef70904569fccccae96
636-noura <66725060+636-noura@users.noreply.github.com>
2020-06-16 00:05:53 +0300

    hi

--- ios-hw-1.playground/Contents.swift
+++ ios-hw-1.playground/Contents.swift
@@ -5,17 +5,17 @@
  
  */
 
-let name = "Ahmad"
+var name = "Ahmad"
 
-name = "Salem"
+ name = "Salem"
 
 
 var myWeight = 60
-var FriendWeight = 80.5
-var height = 1.5
+var FriendWeight = 80
+var height = 159
 
 
-var weightSum = myWeight + FriendWeight
+var weightSum = (myWeight + FriendWeight)
 
 
 
@@ -38,33 +38,41 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "نورا"
+var lastName1 = "حمد"
+var email1 = "noura@gmail.com"
+var phoneNumber1 = "99732647"
+var age1 = 17
+var country1 = "الكويت"
+var area1 = "الفروانية"
+var password = "0000"
+var isKuwaiti = true
 
 //Person 2
-
-
-
-
-
+var firstName2 = "دينا"
+var lastName2 = "أنور"
+var email2 = "dina@gmail.com"
+var phoneNumber2 = "99732647"
+var age2 = 18
+var country2 = "الكويت"
+var area2 = "الفروانية"
+var password2 = "0000"
+var isKuwaiti2 = true
 
 
 
 
 
 //Person 3
-
-
-
-
-
+var firstName3 = "بشاير"
+var lastName3 = "علي"
+var email3 = "hi@gmail.com"
+var phoneNumber3 = "99732647"
+var age3 = 17
+var country3 = "الكويت"
+var area3 = "الفر،انية"
+var password3 = "0000"
+var isKuwaiti3 = true
 
 
 
@@ -82,3 +90,17 @@ var isKuwaiti = false
 
  
  */
+age1 == age2
+area1 == area3
+age3 > 18
+age2 >= 18
+age1 > 18
+age1 < 18
+age2 < 18
+age3 < 18
+age2 == 17
+age3 == 17
+age1 == 17
+age2 != 17
+age3 != 17 
+
